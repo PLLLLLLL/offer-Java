a732e8c5ca83173d9d44e4a775b33af2d564ca95
PLLLLLLL <34536380+PLLLLLLL@users.noreply.github.com>
2019-05-29 15:21:50 +0800

    代码测试通过

--- 3-数组中重复的数字.java
+++ 3-数组中重复的数字.java
@@ -64,3 +64,38 @@ public class Solution {
         return;
     }
 }
+
+
+或者：
+
+
+public class Solution {
+    // Parameters:
+    //    numbers:     an array of integers
+    //    length:      the length of array numbers
+    //    duplication: (Output) the duplicated number in the array number,length of duplication array is 1,so using duplication[0] = ? in implementation;
+    //                  Here duplication like pointor in C/C++, duplication[0] equal *duplication in C/C++
+    //    这里要特别注意~返回任意重复的一个，赋值duplication[0]
+    // Return value:       true if the input is valid, and there are some duplications in the array number
+    //                     otherwise false
+    public boolean duplicate(int numbers[],int length,int [] duplication) {
+        if (numbers == null || length<=0)
+			return false;
+		
+        int tempVal = 0;
+		
+        for (int i=0;i<numbers.length;i++) {
+			while (numbers[i]!=i) {
+				if (numbers[i]==numbers[numbers[i]]) {
+					//System.out.println(numbers[i]);
+					duplication[0] = numbers[i];
+					return true;
+				}
+				tempVal = numbers[i];
+				numbers[i] = numbers[tempVal];
+				numbers[tempVal] = tempVal;
+			}
+		}
+		return false;
+    }
+}
