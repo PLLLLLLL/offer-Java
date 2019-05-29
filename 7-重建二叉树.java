08d5a1d4eef17f05fad55309500789e9632d6c1e
PLLLLLLL <34536380+PLLLLLLL@users.noreply.github.com>
2019-05-29 17:07:03 +0800

    代码测试通过

--- 7-重建二叉树.java
+++ 7-重建二叉树.java
@@ -28,12 +28,12 @@ public class Solution {
     public TreeNode reConstructBinaryTree(int [] preArr,int [] inArr) {
         // 输入的合法性判断，两个数组都不能为空
         if (preArr == null || inArr == null) {
-			return null;
-		}
+            return null;
+	}
 		
         TreeNode root = reConstruct(preArr, 0, preArr.length-1, inArr, 0, inArr.length-1);
  
-		return root;
+	return root;
 	}
 	
     /**
@@ -45,29 +45,29 @@ public class Solution {
      * @param inE    中序遍历的结束位置
      * @return 树的根结点
      */
-	public TreeNode reConstruct(int[] preArr, int preS, int preE, int[] inArr, int inS, int inE) {
-		    // 开始位置大于结束位置说明已经没有需要处理的元素了
+    public TreeNode reConstruct(int[] preArr, int preS, int preE, int[] inArr, int inS, int inE) {
+        // 开始位置大于结束位置说明已经没有需要处理的元素了
         if (preS>preE || inS>inE) {
             return null;
-		    }
-        // 取前序遍历的第一个数字preArr[preS]，就是当前的根结点
-		    TreeNode root = new TreeNode(preArr[preS]);
-		    for (int i=inS; i<=inE; i++) {
-			      if (inArr[i]==preArr[preS]) {
-				        // 记录 len，用于递归时确定 前序遍历序列的截取长度
-				        int len = i - inS;
+        }
+	// 取前序遍历的第一个数字preArr[preS]，就是当前的根结点
+        TreeNode root = new TreeNode(preArr[preS]);
+        for (int i=inS; i<=inE; i++) {
+	    if (inArr[i]==preArr[preS]) {
+		// 记录 len，用于递归时确定 前序遍历序列的截取长度
+		int len = i - inS;
 				        
-				        // 递归构建当前根结点的左子树，左子树的元素个数：len 个
+		// 递归构建当前根结点的左子树，左子树的元素个数：len 个
                 // 左子树对应的前序遍历的位置在[preS+1, preS+len]
                 // 左子树对应的中序遍历的位置在[inS, i-1]
-				        root.left = reConstruct(preArr, preS+1, preS+len, inArr, inS, i-1);
+		root.left = reConstruct(preArr, preS+1, preS+len, inArr, inS, i-1);
                 // 递归构建当前根结点的右子树，右子树的元素个数：inE-i 个
                 // 右子树对应的前序遍历的位置在[preS+len+1, preE]
                 // 右子树对应的中序遍历的位置在[i+1, inE]
-				        root.right = reConstruct(preArr, preS+len+1, preE, inArr, i+1, inE);
-			      }			
-	    	}
-		    System.out.print(root.val+" ");
-		    return root;
+		root.right = reConstruct(preArr, preS+len+1, preE, inArr, i+1, inE);
+	    }			
+	}
+	System.out.print(root.val+" ");
+        return root;
     }
 }
