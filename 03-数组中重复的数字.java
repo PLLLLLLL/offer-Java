24a6013121d4927466e138404af87e2424c15509
PLLLLLLL <34536380+PLLLLLLL@users.noreply.github.com>
2019-05-30 11:15:06 +0800

    代码测试通过

--- 03-数组中重复的数字.java
+++ 03-数组中重复的数字.java
@@ -65,9 +65,9 @@ public class Solution {
     }
 }
 
-
-或者：
-
+/**
+  *或者如下
+  */
 
 public class Solution {
     // Parameters:
@@ -76,27 +76,24 @@ public class Solution {
     //    duplication: (Output) the duplicated number in the array number,length of duplication array is 1,so using duplication[0] = ? in implementation;
     //                  Here duplication like pointor in C/C++, duplication[0] equal *duplication in C/C++
     //    这里要特别注意~返回任意重复的一个，赋值duplication[0]
-    //    Return value:       true if the input is valid, and there are some duplications in the array number
+    // Return value:       true if the input is valid, and there are some duplications in the array number
     //                     otherwise false
     public boolean duplicate(int numbers[],int length,int [] duplication) {
         if (numbers == null || length<=0)
-            return false;
-		
-        int tempVal = 0;
-		
-        for (int i=0;i<numbers.length;i++) {
-            while (numbers[i]!=i) {
-	            if (numbers[i]==numbers[numbers[i]]) {
-		            //System.out.println(numbers[i]);
-		            duplication[0] = numbers[i];
-		            return true;
-                }
-		    
-		        tempVal = numbers[i];
-		        numbers[i] = numbers[tempVal];
-		        numbers[tempVal] = tempVal;
-	        }
-        }
-        return false;
+			return false;
+		int tempVal = 0;
+		for (int i=0;i<numbers.length;i++) {
+			while (numbers[i]!=i) {
+				if (numbers[i]==numbers[numbers[i]]) {
+					//System.out.println(numbers[i]);
+					duplication[0] = numbers[i];
+					return true;
+				}
+				tempVal = numbers[i];
+				numbers[i] = numbers[tempVal];
+				numbers[tempVal] = tempVal;
+			}
+		}
+		return false;
     }
 }
